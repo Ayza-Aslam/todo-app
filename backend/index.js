8feb1562a10fb8e0a34ff843b9a767e7d476c730
Ayza Aslam <itzaizaaslam@gmail.com>
2026-06-20 15:49:51 +0500

    fix railway database connection

--- backend/index.js
+++ backend/index.js
@@ -7,15 +7,8 @@ app.use(cors());
 app.use(express.json());
 
 const pool = new Pool({
-connectionString: process.env.DATABASE_URL || undefined,
-ssl: process.env.DATABASE_URL
-  ? { rejectUnauthorized: false }
-  : false,
-user: process.env.DB_USER,
-host: process.env.DB_HOST,
-database: process.env.DB_NAME,
-password: process.env.DB_PASSWORD,
-port: process.env.DB_PORT
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
 });
 
 pool.query(`  CREATE TABLE IF NOT EXISTS todos (
